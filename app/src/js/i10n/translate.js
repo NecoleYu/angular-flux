6111b6b876bc4aa96db3e2fd7cf280c4a45b273b
Nicholas Irving <nirving@darkedges.com>
2014-12-28 22:16:38 +1100

    Nicholas Irving: Fixed * Determining Preferred Language

--- app/src/js/i10n/translate.js
+++ app/src/js/i10n/translate.js
@@ -1,21 +1,21 @@
 (function () {
     angular.module("app").config(['$translateProvider',
         function ($translateProvider) {
-            $translateProvider.translations('en_US', {
+            $translateProvider.translations('en', {
                 "todo.input": "Add a new memo",
                 "todo.create": "Add",
                 "todo.loading": "Loading ....",
                 "edit.text": "Please enter a todo",
-                "edit.save":"Save"
+                "edit.save": "Save"
             });
-            $translateProvider.translations('zh_CN', {
+            $translateProvider.translations('zh', {
                 "todo.input": "增加新的备忘录",
                 "todo.create": "添加",
                 "todo.loading": "正在加载....",
                 "edit.text": "请输入todo",
-                "edit.save":"保存"
-
+                "edit.save": "保存"
             });
-            $translateProvider.preferredLanguage('en_US');
+            $translateProvider.registerAvailableLanguageKeys(['en', 'zh'], {});
+            $translateProvider.determinePreferredLanguage();
   }]);
 })();
